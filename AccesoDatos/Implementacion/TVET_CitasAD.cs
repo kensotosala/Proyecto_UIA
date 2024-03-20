3033ed64fd369271317352a763127972342d1f1e
Your Name <you@example.com>
2024-03-20 11:24:53 -0600

    Implementación TVET_CitasAD

--- AccesoDatos/Implementacion/TVET_CitasAD.cs
+++ AccesoDatos/Implementacion/TVET_CitasAD.cs
@@ -4,25 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AccesoDatos.Implementacion
 {
-    public class TVET_DetalleMascotaAD : ITVET_DetalleMascotaAD
+    public class TVET_CitasAD : ITVET_CitasAD
     {
         private VETEntidades _vETEntidades;
 
-        public TVET_DetalleMascotaAD(VETEntidades vETEntidades)
+        public TVET_CitasAD(VETEntidades vETEntidades)
         {
             _vETEntidades = vETEntidades;
         }
 
-        public List<TVET_DetalleMascota> obtenerDetalleMascota_ENT()
+        public List<TVET_Citas> obtenerCita_ENT()
         {
-            List<TVET_DetalleMascota> listaDetalles = new List<TVET_DetalleMascota>();
+            List<TVET_Citas> listaCitas = new List<TVET_Citas>();
             try
             {
                 _vETEntidades.Configuration.ProxyCreationEnabled = false;
-                listaDetalles = _vETEntidades.TVET_DetalleMascota.ToList();
+                listaCitas = _vETEntidades.TVET_Citas.ToList();
             }
             catch (Exception ex)
             {
@@ -33,17 +35,17 @@ namespace AccesoDatos.Implementacion
                 _vETEntidades.Configuration.ProxyCreationEnabled = true;
             }
 
-            return listaDetalles;
+            return listaCitas;
         }
 
-        public TVET_DetalleMascota obtenerDetalleMascotaXId_ENT(int pDetalle)
+        public TVET_Citas obtenerCitaXId_ENT(int pCita)
         {
-            TVET_DetalleMascota detalle = new TVET_DetalleMascota();
+            TVET_Citas detalle = new TVET_Citas();
 
             try
             {
                 _vETEntidades.Configuration.ProxyCreationEnabled = false;
-                detalle = _vETEntidades.TVET_DetalleMascota.ToList().Find(x => x.TN_IdMascota == pDetalle);
+                detalle = _vETEntidades.TVET_Citas.ToList().Find(x => x.TN_IdCita == pCita);
             }
             catch (Exception ex)
             {
@@ -57,17 +59,17 @@ namespace AccesoDatos.Implementacion
             return detalle;
         }
 
-        public bool agregarDetalleMascota_ENT(TVET_DetalleMascota pDetalle)
+        public bool agregarCita_ENT(TVET_Citas pCita)
         {
             bool estado = false;
             try
             {
                 _vETEntidades.Configuration.ProxyCreationEnabled = false;
-                var registroEncontrado = _vETEntidades.TVET_DetalleMascota.Find(pDetalle.TN_IdMascota);
+                var registroEncontrado = _vETEntidades.TVET_Citas.Find(pCita.TN_IdCita);
 
                 if (registroEncontrado == null)
                 {
-                    TVET_DetalleMascota tVET_DetallesMascota = _vETEntidades.TVET_DetalleMascota.Add(pDetalle);
+                    TVET_Citas tVET_Citas = _vETEntidades.TVET_Citas.Add(pCita);
                     _vETEntidades.SaveChanges();
                     estado = true;
                 }
@@ -83,16 +85,16 @@ namespace AccesoDatos.Implementacion
             return estado;
         }
 
-        public bool modificarDetalleMascota_ENT(TVET_DetalleMascota pDetalle)
+        public bool modificarCita_ENT(TVET_Citas pCita)
         {
             bool estado = false;
             try
             {
                 _vETEntidades.Configuration.ProxyCreationEnabled = false;
-                var registroEncontrado = _vETEntidades.TVET_DetalleMascota.Find(pDetalle.TN_IdMascota);
+                var registroEncontrado = _vETEntidades.TVET_DetalleMascota.Find(pCita.TN_IdCita);
                 if (registroEncontrado != null)
                 {
-                    _vETEntidades.Entry(registroEncontrado).CurrentValues.SetValues(pDetalle);
+                    _vETEntidades.Entry(registroEncontrado).CurrentValues.SetValues(pCita);
                     _vETEntidades.Entry(registroEncontrado).State = EntityState.Modified;
                     _vETEntidades.SaveChanges();
                     estado = true;
@@ -109,16 +111,16 @@ namespace AccesoDatos.Implementacion
             return estado;
         }
 
-        public bool eliminarDetalleMascota_ENT(TVET_DetalleMascota pDetalle)
+        public bool eliminarCita_ENT(TVET_Citas pCita)
         {
             bool estado = false;
             try
             {
                 _vETEntidades.Configuration.ProxyCreationEnabled = false;
-                var registroEncontrado = _vETEntidades.TVET_DetalleMascota.Find(pDetalle.TN_IdMascota);
+                var registroEncontrado = _vETEntidades.TVET_Citas.Find(pCita.TN_IdCita);
                 if (registroEncontrado != null)
                 {
-                    _vETEntidades.Entry(registroEncontrado).CurrentValues.SetValues(pDetalle);
+                    _vETEntidades.Entry(registroEncontrado).CurrentValues.SetValues(pCita);
                     _vETEntidades.Entry(registroEncontrado).State = EntityState.Deleted;
                     _vETEntidades.SaveChanges();
                     estado = true;
